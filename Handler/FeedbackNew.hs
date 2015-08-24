532691abf72433efc38d49de63802527ec325f71
Ivan Garcia <ivan.garcia@unosquare.com>
2015-08-23 18:01:14 -0700

    Initial commit.

---
+++ Handler/FeedbackNew.hs
@@ -0,0 +1,6 @@
+module Handler.FeedbackNew where
+
+import Import
+
+getFeedbackNewR :: Handler Html
+getFeedbackNewR = error "Not yet implemented: getFeedbackNewR"
---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "gotfeedback" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
